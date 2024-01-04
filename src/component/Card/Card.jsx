604f057c5dee8614d68f02f1b7fd6815baf8dc31
Taegyun <taegyun98@naver.com>
2024-01-05 00:28:55 +0900

    CARD 수정

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { React, useState } from "react";
 import img from "../../Img/Icons/AccordionArrow.svg";
 import "./Card.css";
 
 export default function Card(props) {
+  const [cardhover, setCardhover] = useState(false);
+
   return (
-    <div className={`cardContainer ${props.type}`}>
+    <div
+      className={`cardContainer ${props.type}`}
+      // onMouseEnter={(e) => {
+      //   setCardhover(true);
+      //   console.log(cardhover);
+      // }}
+      // onMouseOut={(e) => {
+      //   setCardhover(false);
+      //   console.log(cardhover);
+      // }}
+    >
       <div
         className="cardImg"
         style={{
@@ -13,8 +25,12 @@ export default function Card(props) {
       ></div>
       <div className="cardinfoContainer">
         <div className="cardTextContainer">
-          <div className="cardTitle">{props.title || "sad"}</div>
-          <div className="cardIntro">{props.info || "ss"}</div>
+          <div className={`cardTitle `}>
+            <span>{props.title || "sad"}</span>
+          </div>
+          <div className="cardIntro">
+            <span>{props.info || "ss"}</span>
+          </div>
         </div>
         <div className="cardMore">
           <span>자세히보기 </span>
